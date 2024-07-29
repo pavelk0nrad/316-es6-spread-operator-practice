d3dce84079ca670e13ac7a8d1a2b19e3da7d7e6f
Pavel Konrad <115944509+pavelk0nrad@users.noreply.github.com>
2024-07-29 10:58:22 +0200

    add item funcionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [newItem, setItem] = useState("")
+  const [itemList, setItemList] = useState([])
+
+  function changeHandler(event){
+
+    const value =  event.target.value;
+
+    setItem(value)
+  
+
+    console.log(value)
+    }
+
+    function addItem() {
+      setItemList(prevValue => {
+        return[
+          ...prevValue,
+          newItem
+        ]
+
+      })
+      setItem("")
+
+  }
+    
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
-          <span>Add</span>
+        <input type="text" onChange={changeHandler} />
+        <button onClick={addItem}>
+          <span>add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {itemList.map(items =>  <li> {items} </li>  )}
         </ul>
       </div>
     </div>
